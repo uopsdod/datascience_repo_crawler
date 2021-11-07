ded5f1abdd47c4fedc6b4c3e3fa3c8b298dda847
uopsdod <uopsdod@gmail.com>
2021-11-07 12:23:11 -0800

    finish balancing dataset

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+from myutils.datasetservice import DatasetService
 from myutils.printservice import PrintService
 from training_model import ModelDecisionTree
 from training_model.naivebayes import ModelNaiveBayes
@@ -5,137 +8,146 @@ from training_model.randomforest import ModelRandomForest
 from training_model.svc import ModelSVC
 
 
+
 class Main:
     def __init__(self):
         self.printService = PrintService()
+        self.datasetService = DatasetService()
         pass
 
     def start(self):
 
-        while(True):
-            print("choose intention: ")
-            print("1. retrain models ")
-            print("2. see specific results")
-            print("3. see all results")
-            print("4. exit")
-            operation_id = input("please type # of intention: ").strip()
-            if operation_id != "1" and operation_id != "2" and operation_id != "3" and operation_id != "4" and operation_id != "gap":
-                print("intention_id is unknown. abort operation")
-                continue
-            elif operation_id == "3":
-                self.printService.print_all_test_result()
-                continue
-            elif operation_id == "4":
-                print("program ended")
-                break
-            elif operation_id == "gap":
-                self.printService.print_all_test_gap_result()
-                continue
-            operation_picked = self.get_intention_picked(operation_id)
+        dataset_types = ["bug", "feature", "rating", "userexperience"] # add two more
+        for dataset_type in dataset_types:
+            # dataset_type = "bug"
+            features_gleaned = ["title", "comment", "label"]
+            df = self.datasetService.load_file(dataset_type, features_gleaned)
+            is_dataset_balance = self.datasetService.is_dataset_balance(dataset_type, df, "label")
+            print("hey001")
 
-            print("choose operation: ")
-            print("1. generate training results ")
-            print("2. compare training results")
-            operation_id = input("please type # of operation: ").strip()
-            if operation_id != "1" and operation_id != "2":
-                print("operation_id is unknown. abort operation")
-                continue
+        # while(True):
+        #     print("choose intention: ")
+        #     print("1. retrain models ")
+        #     print("2. see specific results")
+        #     print("3. see all results")
+        #     print("4. exit")
+        #     operation_id = input("please type # of intention: ").strip()
+        #     if operation_id != "1" and operation_id != "2" and operation_id != "3" and operation_id != "4" and operation_id != "gap":
+        #         print("intention_id is unknown. abort operation")
+        #         continue
+        #     elif operation_id == "3":
+        #         self.printService.print_all_test_result()
+        #         continue
+        #     elif operation_id == "4":
+        #         print("program ended")
+        #         break
+        #     elif operation_id == "gap":
+        #         self.printService.print_all_test_gap_result()
+        #         continue
+        #     operation_picked = self.get_intention_picked(operation_id)
+        #
+        #     print("choose operation: ")
+        #     print("1. generate training results ")
+        #     print("2. compare training results")
+        #     operation_id = input("please type # of operation: ").strip()
+        #     if operation_id != "1" and operation_id != "2":
+        #         print("operation_id is unknown. abort operation")
+        #         continue
+        #
+        #     print("choose model: ")
+        #     print("1. decision tree ")
+        #     print("2. random forest ")
+        #     print("3. naïve bayes ")
+        #     print("4. LinearSVC ")
+        #     model_id = input("please type # of model: ").strip()
+        #     # print(f'You entered {model_id}')
+        #     # print("please type # of model: ")
+        #     model_picked = self.get_model_picked(model_id);
+        #     if model_picked == "none":
+        #         print("model id is unknown. abort operation")
+        #         continue
+        #
+        #     if operation_id == "1":
+        #         self.generate_training(model_picked, operation_picked)
+        #     elif operation_id == "2":
+        #         self.compare_training(model_picked, operation_picked)
 
-            print("choose model: ")
-            print("1. decision tree ")
-            print("2. random forest ")
-            print("3. naïve bayes ")
-            print("4. LinearSVC ")
-            model_id = input("please type # of model: ").strip()
-            # print(f'You entered {model_id}')
-            # print("please type # of model: ")
-            model_picked = self.get_model_picked(model_id);
-            if model_picked == "none":
-                print("model id is unknown. abort operation")
-                continue
-
-            if operation_id == "1":
-                self.generate_training(model_picked, operation_picked)
-            elif operation_id == "2":
-                self.compare_training(model_picked, operation_picked)
-
-    def compare_training(self, model_picked, operation_picked):
-        print("choose smell: ")
-        print("1. Feature Envy ")
-        print("2. Data Class ")
-        print("3. God Class ")
-        print("4. Long Method ")
-        print("(ex. 1) - for feature envy")
-        smell_ids = input("please type # of smell: ").strip()
-        smell_picked = self.get_smell_picked(smell_ids)
-        if smell_picked == "none":
-            print("smell id is unknown. abort operation")
-            return
-        elif len(smell_picked) > 1:
-            print("only one smell id is allowed. abort operation")
-            return
-
-        # God Class, Data Class, Feature Envy, and Long Method
-        model_picked.train(smell_picked, "compare", operation_picked)
-
-    def generate_training(self, model_picked, operation_picked):
-
-        print("choose smells: ")
-        print("1. Feature Envy ")
-        print("2. Data Class ")
-        print("3. God Class ")
-        print("4. Long Method ")
-        print("(ex. 1) - for feature envy")
-        print("(ex. 1 3) - for feature envy, god class")
-        print("(ex. 1 3 4) - for feature envy, god class, long method")
-        smell_ids = input("please type # of one or more smells: ").strip()
-        smell_picked = self.get_smell_picked(smell_ids)
-        if smell_picked == "none":
-            print("smell id is unknown. abort operation")
-            return
-
-        # God Class, Data Class, Feature Envy, and Long Method
-        model_picked.train(smell_picked, "generate", operation_picked)
-
-    def get_intention_picked(self, intention_id):
-        if intention_id == "1":
-            return "retrain"
-        if intention_id == "2":
-            return "lookup"
-        return "none"
-
-    def get_model_picked(self, model_id):
-        if model_id == "1":
-            return ModelDecisionTree()
-        if model_id == "2":
-            return ModelRandomForest()
-        if model_id == "3":
-            return ModelNaiveBayes()
-        if model_id == "4":
-            return ModelSVC()
-        return "none"
-
-    def get_smell_picked(self, smell_ids):
-        smell_str_array = []
-        smell_ids_array = smell_ids.split(" ")
-
-        for smell_id in smell_ids_array:
-            if smell_id == "1":
-                smell_str_array.append("feature-envy")
-            elif smell_id == "2":
-                smell_str_array.append("data-class")
-            elif smell_id == "3":
-                smell_str_array.append("god-class")
-            elif smell_id == "4":
-                smell_str_array.append("long-method")
-            else:
-                return "none"
-
-        if len(smell_str_array) == 0:
-            return "none"
-
-        return smell_str_array
-
+    # def compare_training(self, model_picked, operation_picked):
+    #     print("choose smell: ")
+    #     print("1. Feature Envy ")
+    #     print("2. Data Class ")
+    #     print("3. God Class ")
+    #     print("4. Long Method ")
+    #     print("(ex. 1) - for feature envy")
+    #     smell_ids = input("please type # of smell: ").strip()
+    #     smell_picked = self.get_smell_picked(smell_ids)
+    #     if smell_picked == "none":
+    #         print("smell id is unknown. abort operation")
+    #         return
+    #     elif len(smell_picked) > 1:
+    #         print("only one smell id is allowed. abort operation")
+    #         return
+    #
+    #     # God Class, Data Class, Feature Envy, and Long Method
+    #     model_picked.train(smell_picked, "compare", operation_picked)
+    #
+    # def generate_training(self, model_picked, operation_picked):
+    #
+    #     print("choose smells: ")
+    #     print("1. Feature Envy ")
+    #     print("2. Data Class ")
+    #     print("3. God Class ")
+    #     print("4. Long Method ")
+    #     print("(ex. 1) - for feature envy")
+    #     print("(ex. 1 3) - for feature envy, god class")
+    #     print("(ex. 1 3 4) - for feature envy, god class, long method")
+    #     smell_ids = input("please type # of one or more smells: ").strip()
+    #     smell_picked = self.get_smell_picked(smell_ids)
+    #     if smell_picked == "none":
+    #         print("smell id is unknown. abort operation")
+    #         return
+    #
+    #     # God Class, Data Class, Feature Envy, and Long Method
+    #     model_picked.train(smell_picked, "generate", operation_picked)
+    #
+    # def get_intention_picked(self, intention_id):
+    #     if intention_id == "1":
+    #         return "retrain"
+    #     if intention_id == "2":
+    #         return "lookup"
+    #     return "none"
+    #
+    # def get_model_picked(self, model_id):
+    #     if model_id == "1":
+    #         return ModelDecisionTree()
+    #     if model_id == "2":
+    #         return ModelRandomForest()
+    #     if model_id == "3":
+    #         return ModelNaiveBayes()
+    #     if model_id == "4":
+    #         return ModelSVC()
+    #     return "none"
+    #
+    # def get_smell_picked(self, smell_ids):
+    #     smell_str_array = []
+    #     smell_ids_array = smell_ids.split(" ")
+    #
+    #     for smell_id in smell_ids_array:
+    #         if smell_id == "1":
+    #             smell_str_array.append("feature-envy")
+    #         elif smell_id == "2":
+    #             smell_str_array.append("data-class")
+    #         elif smell_id == "3":
+    #             smell_str_array.append("god-class")
+    #         elif smell_id == "4":
+    #             smell_str_array.append("long-method")
+    #         else:
+    #             return "none"
+    #
+    #     if len(smell_str_array) == 0:
+    #         return "none"
+    #
+    #     return smell_str_array
 
 # entry point
 main = Main();
---
+++ myutils/datasetservice.py
@@ -0,0 +1,96 @@
+import json
+
+import pandas as pd
+from sklearn.utils import resample
+
+
+class DatasetService:
+    def __init__(self):
+        pass
+
+    def load_file(self, dataset_type, elem_keys):
+        file_path = self._get_dataset_filename(dataset_type)
+        with open(file_path) as json_file:
+            data_raw = json.load(json_file)
+            data_gleaned = self._glean_data(data_raw, elem_keys)
+            df = pd.DataFrame(data_gleaned, columns = elem_keys)
+            return df
+
+    def is_dataset_balance(self, dataset_type, df, class_y):
+        class1_count, class2_count = self._get_class_count(df, class_y)
+
+        if (class1_count != class2_count):
+            print(f'unbalanced {dataset_type}: {class1_count} vs {class2_count} ')
+
+            df = self.upsample(df, class_y)
+            class1_count, class2_count = self._get_class_count(df, class_y)
+
+            print(f'upsampled {dataset_type}: {class1_count} vs {class2_count} ')
+            return False
+        return True
+
+    def _get_class_count(self, df, class_y):
+        value_counts = df[class_y].value_counts()
+        class1_count = value_counts[0]
+        class2_count = value_counts[1]
+        return class1_count, class2_count
+
+    def upsample(self, df, class_y):
+        value_counts = df[class_y].value_counts()
+        value_counts_key = value_counts.keys()
+        key1 = value_counts_key[0];
+        key2 = value_counts_key[1];
+
+        class1_count = value_counts[0]
+        class2_count = value_counts[1]
+
+        class_count_max = max(class1_count, class2_count)
+
+        # Separate majority and minority classes
+        if (class1_count < class2_count):
+            df_majority = df[df[class_y]==key2]
+            df_minority = df[df[class_y]==key1]
+        else:
+            df_majority = df[df[class_y]==key1]
+            df_minority = df[df[class_y]==key2]
+
+        # Upsample minority class
+        df_minority_upsampled = resample(df_minority,
+                                         replace=True,                 # sample with replacement
+                                         n_samples=class_count_max,    # to match majority class
+                                         random_state=123)             # reproducible results
+
+        # Combine majority class with upsampled minority class
+        df_upsampled = pd.concat([df_majority, df_minority_upsampled])
+
+        # Display new class counts
+        # print(df_upsampled[class_y].value_counts())
+
+        return df_upsampled
+
+
+    def _get_dataset_filename(self, dataset_type):
+        dateset_folder = "training_dataset"
+        if dataset_type == "bug":
+            dateset_file = "Bug_tt.json"
+        elif dataset_type == "feature":
+            dateset_file = "Feature_tt.json"
+        elif dataset_type == "rating":
+            dateset_file = "Rating_tt.json"
+        elif dataset_type == "userexperience":
+            dateset_file = "UserExperience_tt.json"
+        else:
+            return "none"
+
+        return dateset_folder + "/" + dateset_file
+
+    def _glean_data(self, json_file, elem_keys):
+        data = []
+        for elem in json_file:
+            example = []
+            for elem_key in elem_keys:
+                example.append(elem[elem_key])
+            data.append(example)
+        return data
+
+
